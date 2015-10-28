0598e38e3904e4cdf41665d1cce8555a9cbda9ff
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-10-28 12:25:41 -0700

    add simple react view

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
 function counter(state = 0, action) {
 	switch(action.type) {
@@ -24,3 +26,46 @@ store.dispatch({type: 'INCREMENT'});
 store.dispatch({type: 'DECREMENT'});
 store.dispatch({type: 'INCREMENT'});
 store.dispatch({type: 'INCREMENT'});
+
+class App extends Component {
+	constructor(props) {
+		super();
+		this.state = { value: props.value };
+	}
+
+	render() {
+		return (
+			<div>
+				<Display value={this.state.value} />
+				<Control />
+			</div>
+		);
+	}
+}
+
+class Display extends Component {
+	render() {
+		return (
+			<div>
+				{this.props.value}
+			</div>
+		);
+	}	
+}
+
+class Control extends Component {
+	render() {
+		return (
+			<div>
+				<button>+
+				</button>
+				<button>-
+				</button>
+			</div>
+		);
+	}
+}
+render(
+	<App value={0} />,
+	document.getElementById('root')
+);
